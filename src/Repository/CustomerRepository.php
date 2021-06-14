43ed3fd61c16d30c5beaac0be810bedf4fe036e9
zlatkos94 <zlatkospajic94@gmail.com>
2021-06-14 14:48:26 +0200

    first commit

---
+++ src/Repository/CustomerRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Customer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Customer|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Customer|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Customer[]    findAll()
+ * @method Customer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CustomerRepository extends ServiceEntityRepository
+{
+    private $manager;
+
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $entityManager)
+    {
+        parent::__construct($registry, Customer::class);
+        $this->manager=$entityManager;
+    }
+
+    public function saveCustomer($firstName, $lastName, $email, $phoneNumber)
+    {
+        $newCustomer = new Customer();
+
+        $newCustomer
+            ->setFirstName($firstName)
+            ->setLastName($lastName)
+            ->setEmail($email)
+            ->setPhoneNumber($phoneNumber);
+
+        $this->manager->persist($newCustomer);
+        $this->manager->flush();
+
+    }
+
+    public function updateCustomer(Customer $customer, $data) {
+
+        empty($data['firstName']) ? true : $customer->setFirstName($data['firstName']);
+        empty($data['lastName']) ? true : $customer->setLastName($data['lastName']);
+        empty($data['email']) ? true : $customer->setEmail($data['email']);
+        empty($data['phoneNumber']) ? true : $customer->setPhoneNumber($data['phoneNumber']);
+
+        $this->manager->flush();
+    }
+
+    // /**
+    //  * @return Customer[] Returns an array of Customer objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Customer
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
